6ec45b4764c93b68e35c4551050fd9cf41813ecd
Aviral Agarwal <aviralagarwal25@gmail.com>
2019-11-15 13:11:09 +0530

    Refactor driver distribution

--- src/com/Dryft/MainThread.java
+++ src/com/Dryft/MainThread.java
@@ -2,12 +2,54 @@ package com.Dryft;
 
 import com.Dryft.utils.DBConn;
 import com.Dryft.gui.SignIn;
+import com.Dryft.models.Car;
+import com.Dryft.models.Ride;
+import com.Dryft.models.Location;
+import com.Dryft.DAOs.LocationDAO;
+import com.Dryft.DAOs.CarDAO;
 
 import java.lang.Thread;
 import java.io.IOException;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+class DriverThread extends Thread {
+
+    private int id;
+    private Location origin;
+    private Location homeLocation;
+    private Car car;
+
+    DriverThread(int id, Location origin, Location homeLocation, Car car) {
+        this.id = id;
+        this.origin = origin;
+        this.homeLocation = homeLocation;
+        this.car = car;
+    }
+
+    public void run() {
+        try{
+            Connection conn = DBConn.getConn();
+            PreparedStatement st = conn.prepareStatement("Update drivers set onRoad = (?) where id = (?) ;");
+            st.setBoolean(1, true);
+            st.setInt(2, id);
+            st.executeUpdate();
+            int time = Ride.calculateDistance(origin, homeLocation) / car.getSpeed();
+            Thread.sleep(time);
+            PreparedStatement stm = conn
+                    .prepareStatement("Update drivers set onRoad = (?) and location = homeLocation where id = (?) ;");
+            stm.setBoolean(1, false);
+            stm.setInt(2, id);
+            stm.executeUpdate();
+            DBConn.closeConn();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
 
 class DistributionThread extends Thread {
 
@@ -16,13 +58,24 @@ class DistributionThread extends Thread {
             Thread.sleep(300);
             System.out.println("Starting Driver Redistribution");
             Connection conn = DBConn.getConn();
-            PreparedStatement st = conn.prepareStatement("UPDATE drivers set location = homeLocation Where onRoad = (?) ;");
+            PreparedStatement st = conn.prepareStatement("Select id, location, homeLocation, carNumber from drivers Where onRoad = (?) ;");
             st.setBoolean(1, false);
-            st.executeUpdate();
+            ResultSet result = st.executeQuery();
+            while (result.next()) {
+                DriverThread th = new DriverThread(result.getInt("id"),
+                        LocationDAO.getLocation(result.getString("location")),
+                        LocationDAO.getLocation(result.getString("homeLocation")),
+                        CarDAO.getCar(result.getString("carNumber")));
+                th.start();
+            }
             DBConn.closeConn();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void sendDriverBack() {
+
     }
 }
 
@@ -32,26 +85,27 @@ class DistributionThread extends Thread {
 
 public class MainThread {
     public static boolean windowClosed;
+
     public static void main(String[] args) {
-        boolean fileCreated=false;
-        String[] a={};
+        boolean fileCreated = false;
+        String[] a = {};
         SignIn.main(a);
         try {
-            while(true){
+            while (true) {
                 File file = new File("/tmp/running.txt");
                 if (file.createNewFile()) {
-                    fileCreated=true;
+                    fileCreated = true;
                     DistributionThread distribute = new DistributionThread();
                     distribute.setDaemon(true);
                     distribute.start();
                 }
-                if(windowClosed){
+                if (windowClosed) {
                     break;
-                } 
+                }
             }
-            if(fileCreated){
+            if (fileCreated) {
                 File file = new File("/tmp/running.txt");
-                file.delete(); 
+                file.delete();
             }
         } catch (IOException e) {
             e.printStackTrace();
